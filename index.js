8d204b4c8ba9bee33a6e3b6a44b561799650a2b8
Mikola Lysenko <mikolalysenko@gmail.com>
2013-03-04 11:00:17 -0600

    adding files

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function invertPermutation(pi) {
+  var result = new Array(pi.length)
+  for(var i=0; i<pi.length; ++i) {
+    result[pi[i]] = i
+  }
+  return result
+}
+
+module.exports = invertPermutation
